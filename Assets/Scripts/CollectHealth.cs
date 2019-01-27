2cf1c4bb4efcda28da08636dce1dbce013837026
JoshuaSValdez <josh.s.valdez@gmail.com>
2019-01-27 18:09:30 -0500

    Add Health Increase Pickup
    * Added health increase pickup. It's an eight.

---
+++ Assets/Scripts/CollectHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectHealth : MonoBehaviour
+{
+
+    public GameObject playerManager;
+    private PlayerManager playerManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerManagerScript = playerManager.GetComponent<PlayerManager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            playerManagerScript.increaseMaximumHealth();
+            Destroy(gameObject);
+        }
+    }
+}
